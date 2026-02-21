802e44c93ab4eb4ae17ba44c92f3e5c63599ea9d
Yehor P. <egor.potupchyk@gmail.com>
2026-02-21 01:07:48 +0100

    add basic ssl for local testing

--- app/ai-app/ui/chat-web-app/vite.config.ts
+++ app/ai-app/ui/chat-web-app/vite.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig(({mode}) => {
     const env = loadEnv(mode, process.cwd(), '')
@@ -10,12 +11,15 @@ export default defineConfig(({mode}) => {
         plugins: [
             react(),
             tailwindcss(),
+            basicSsl()
         ],
         resolve: {
             dedupe: ["react", "react-dom"],
         },
         envPrefix: ["VITE_", "CHAT_WEB_APP_"],
         server: {
+            https: env.VITE_HTTPS === 'true',
+
             proxy: {
                 '^/api/.*': {
                     target: apiBase,
